9d9c1c68d898e7981adcbfe5e49708012ff0e0ed
Paulo Vieira <paulo.cvieiradev@gmail.com>
2022-08-08 21:06:27 -0300

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles/global.css";
+
+const App = () => {
+  return (
+    <div>
+      <p className="text-4xl">Bem vindo,</p>
+      <p></p>
+    </div>
+  );
+};
+
+export default App;
